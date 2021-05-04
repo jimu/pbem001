153fdc3c58f98f21526345151d382984fa155178
Jim Urbas <git@jimu.net>
2021-05-04 04:08:34 -0500

    Add command class

--- Assets/Scripts/Command.cs
+++ Assets/Scripts/Command.cs
@@ -23,21 +23,7 @@ public class Command
     }
 }
 
-public enum UnitType { Invalid, TB, LB, RB, JB, BB, BPCPI, BPCPS, BPCPN, MISSILE_AA, MISSILE_AG, MISSILE_AM };
-public enum DeploymentType { Invalid, MapEdge, NearBPCP }
 
-[CreateAssetMenu]
-public class UnitTypeData : ScriptableObject
-{
-    public string code;
-    public string strength;
-    public string movementAllowance;
-    public Sprite icon;
-    public int quantity;
-    public DeploymentType deploymentType;
-    public int stackingLimit;
-
-}
 
 
 public class Unit
