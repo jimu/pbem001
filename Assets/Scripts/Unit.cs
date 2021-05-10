f760d5777799663e6b425e32ec2cbf8d63c3a87e
Jim Urbas <git@jimu.net>
2021-05-09 23:46:14 -0500

    Add unit script

---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    public UnitData data;
+
+    public Unit(UnitData data)
+    {
+        this.data = data;
+    }
+}
+
+
