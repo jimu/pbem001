dacdcfff8ca8b2b127c5c441dbff596d0377529e
jimu <jimurbas@gmail.com>
2021-07-29 01:56:21 -0500

    Restore drag map & fix zoom

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -35,7 +35,7 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //HandleMovementInput();
+        HandleMovementInput();
         HandleMouseInput();
         HandleZoom();
     }
